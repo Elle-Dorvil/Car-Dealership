bfeaa368f6f79e1d6c46c7141e7b8a9c891a3790
Elle-Dorvil <rachelle.dorvil@yahoo.com>
2025-05-09 22:09:03 -0400

    Added fields and signatures to Dealership class as well as method signature to DealershipFileManager

--- src/main/java/com/pluralsight/Dealership.java
+++ src/main/java/com/pluralsight/Dealership.java
@@ -1,4 +1,43 @@
 package com.pluralsight;
 
+import java.util.ArrayList;
+
 public class Dealership {
+    private String name;
+    private String address;
+    private String phone;
+
+    ArrayList<Vehicle> vehicles;
+
+    ArrayList<Vehicle> getAllVehicles() {
+
+    }
+
+    ArrayList<Vehicle> getVehiclesByPrice(double min, double max){
+
+    }
+
+    ArrayList<Vehicle> getVehiclesByMakeModel(String make, String model){
+
+    }
+
+    ArrayList<Vehicle> getVehiclesByYear(int min, int max){
+
+    }
+
+    ArrayList<Vehicle> getVehiclesByColor(String color){
+
+    }
+    ArrayList<Vehicle> getVehiclesByMileage(int min, int max){
+
+    }
+    ArrayList<Vehicle> getVehiclesByType(String type){
+
+    }
+    void addVehicle(Vehicle vehicle) {
+
+    }
+    void removeVehicle(int vin) {
+        
+    }
 }
