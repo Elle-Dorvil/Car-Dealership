85d67c1b6995ce4d4db7b3c99d25f609faec2fbf
Elle-Dorvil <rachelle.dorvil@yahoo.com>
2025-05-12 23:21:06 -0400

    Completed UserInterface and ran code

--- src/main/java/com/pluralsight/UserInterface.java
+++ src/main/java/com/pluralsight/UserInterface.java
@@ -1,13 +1,65 @@
 package com.pluralsight;
 
-public class UserInterface {
-    public void display() {
-        private void innit () {
+import java.util.Scanner;
 
+public class UserInterface {
+    private void innit () {
+        DealershipFileManager fileManager = new DealershipFileManager();
+    }
+    public void display() {
+        innit();
+
+        Scanner scanner = new Scanner(System.in);
+        boolean loadOptions = true;
+        while(loadOptions){
+            System.out.println();
+            System.out.println("Welcome to the Car Dealership!");
+            System.out.println("Choose an option:");
+            System.out.println(" 1) Look up by Price Range: ");
+            System.out.println(" 2) Look up by Make Model: ");
+            System.out.println(" 3) Look up by Year: ");
+            System.out.println(" 4) Look up by Color: ");
+            System.out.println(" 5) Look up by Mile Range: ");
+            System.out.println(" 6) Look up by Vehicle Type: ");
+            System.out.println(" 7) List all Vehicles: ");
+            System.out.println(" 8) Add Vehicle: ");
+            System.out.println(" 9) Remove Vehicle: ");
+            System.out.println(" 99) Quit");
+            System.out.print("Your choice: ");
+
+            String input = scanner.nextLine();
+
+            switch(input) {
+                case "1": processGetByPrice();
+                break;
+                case "2": processGetByMakeModel();
+                break;
+                case "3": processGetByYear();
+                break;
+                case "4": processGetByColor();
+                break;
+                case "5": processGetByMileage();
+                break;
+                case "6": processGetByType();
+                break;
+                case "7": processAddVehicle();
+                break;
+                case "8": processRemoveVehicle();
+                break;
+                case "99": loadOptions = false;
+
+                default:
+                    System.out.println("Invalid Choice, Please select another option. ");
+                    return;
+            }
         }
+        scanner.close();
 
     }
     public void processGetByPrice() {
+
+
+
 
     }
     public void processGetByMakeModel() {
@@ -26,16 +78,6 @@ public class UserInterface {
 
     }
     public void processAddVehicle() {
-
-
-
-        Vehicle addVehicle = new Vehicle(vin, year, make, model, vehicleType, color,odometer, price);
-        dealership.addVehicle(newVehicle);
-
-        System.out.println("Vehicle successfully added");
-
-        DealershipFileManager fileManager = new DealershipFileManager();
-        fileManager.saveDealership(dealership);
 
     }
     public void processRemoveVehicle() {
